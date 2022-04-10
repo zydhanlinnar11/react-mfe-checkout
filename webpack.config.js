6571e65c6f8578eb627f03c8f9e9459c5aae6112
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-04-10 22:23:35 +0700

    feat: add domain to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const deps = require('./package.json').dependencies
 module.exports = {
   output: {
-    publicPath: 'http://localhost:3001/',
+    publicPath: 'https://checkout.react-mfe.zydhan.com/',
   },
 
   resolve: {
